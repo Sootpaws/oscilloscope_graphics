2e17d319d0718cd011e338c2b62dfa4ecb1fac0a
Sootpaws <sootpaws@proton.me>
2025-06-25 16:01:13 -0700

    Drawing with VGDL!

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,23 @@
 use oscilli_disp::signal::player::Player;
 use oscilli_disp::linedraw::Drawer;
+use oscilli_disp::vgdl::State;
 use anyhow::Result;
 
 fn main() -> Result<()> {
-    let calibration = vec![
-        // Corners
-        vec![(0.0, 0.1), (0.0, 0.0), (0.1, 0.0)],
-        vec![(0.9, 0.0), (1.0, 0.0), (1.0, 0.1)],
-        vec![(0.0, 0.9), (0.0, 1.0), (0.1, 1.0)],
-        vec![(1.0, 0.9), (1.0, 1.0), (0.9, 1.0)],
-        // Center
-        vec![(0.4, 0.5), (0.6, 0.5)],
-        vec![(0.5, 0.4), (0.5, 0.6)],
-        // Axis
-        vec![(0.75, 0.55), (0.85, 0.45)], vec![(0.75, 0.45), (0.85, 0.55)],
-        vec![(0.45, 0.75), (0.5, 0.8), (0.55, 0.75)], vec![(0.5, 0.8), (0.5, 0.85)]
-    ];
-    let signal = Drawer::new(calibration);
+    let calibration = "draw
+        0.0 0.1  0.0 0.0  0.1 0.0 ,
+        0.9 0.0  1.0 0.0  1.0 0.1 ,
+        0.0 0.9  0.0 1.0  0.1 1.0 ,
+        1.0 0.9  1.0 1.0  0.9 1.0 ,
+        0.4 0.5  0.6 0.5 ,
+        0.5 0.4  0.5 0.6 ,
+        0.75 0.55  0.85 0.45 , 0.75 0.45  0.85 0.55 ,
+        0.45 0.75  0.5 0.8  0.55 0.75 , 0.5 0.8  0.5 0.85 ;
+    ";
+
+    let mut state = State::new();
     let mut player = Player::new()?;
-    player.play(signal);
+    player.play(Drawer::new(state.run(bk)?));
     std::thread::sleep(std::time::Duration::from_secs_f32(100.0));
     Ok(())
 }
